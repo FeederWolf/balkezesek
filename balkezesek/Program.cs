3a2cd5523d3c0bc80cbdd357a2e3d821ecb35ab4
FeederWolf <91455735+FeederWolf@users.noreply.github.com>
2022-03-21 11:36:44 +0100

    5. feladat

--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -18,15 +18,30 @@ namespace balkezesek
             }
 
             Console.WriteLine("3. feladat: " + lista.Count);
-
+            Console.WriteLine("4. feladat: ");
             foreach (var i in lista)
             {
                 if (i.utolso.Date.Year == 1999 && i.utolso.Date.Month == 10)
                 {
-                    Console.WriteLine("{0} , {1:0.00} cm", i.nev, i.magassag * 2.54);
+                    Console.WriteLine("\t{0} , {1:0.0} cm", i.nev, i.magassag * 2.54);
                 }
             }
-
+            Console.WriteLine("5. feladat");
+            bool kerdes = true;
+            while (kerdes == true)
+            {
+                Console.Write("Kérek egy 1990 és 1999 közötti évszámot!: ");
+                int valami = int.Parse(Console.ReadLine());
+                if (valami <= 1990 || valami >= 1999)
+                {
+                    Console.Write("Hibás adat!");
+                }
+                else
+                {
+                    Console.WriteLine("köszönjük");
+                    kerdes = false;
+                }
+            }
 
         }
     }
