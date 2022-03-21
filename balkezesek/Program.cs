feb290fa5bcdf68b6895bd2b2632b5ba28b4969d
FeederWolf <91455735+FeederWolf@users.noreply.github.com>
2022-03-21 11:08:58 +0100

    Add project files.

---
+++ balkezesek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ketbalkezes> lista = new List<ketbalkezes>();
+            foreach (var i in File.ReadAllLines("balkezesek.csv").Skip(1))
+            {
+                lista.Add(new ketbalkezes(i));
+            }
+
+            Console.WriteLine("3. feladat: " + lista.Count);
+        }
+    }
+}
---
+++ balkezesek/ketbalkezes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    class ketbalkezes
+    {
+        /*név;első;utolsó;súly;magasság*/
+
+        public string nev { get; set; }
+        public DateTime elso { get; set; }
+        public DateTime utolso { get; set; }
+
+        public int magassag { get; set; }
+
+        public ketbalkezes(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            nev = s[0];
+            elso = DateTime.Parse(s[1]);
+            utolso = DateTime.Parse(s[2]);
+            magassag = int.Parse(s[3]);
+        }
+
+    }
+}
